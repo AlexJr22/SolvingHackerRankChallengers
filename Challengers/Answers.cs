1892c1bd93f86f8c06b918def3dd6e07a30edbb7
Alex Junior <alexjr2oficial@gmail.com>
2024-03-02 17:50:45 -0300

    BreakingRecords Challenger

--- Challengers/Answers.cs
+++ Challengers/Answers.cs
@@ -60,5 +60,31 @@ namespace SolveHackerRankChallengers.Challengers
             else
                 return "incompatible format";
         }
+
+        public static List<int> BreakingRecords(List<int> scores)
+        {
+            int CountMinScore = 0;
+            int CountMaxScore = 0;
+            int MaxScore = scores[0];
+            int MinScore = scores[0];
+
+            foreach (int i in scores)
+            {
+                if (i > MaxScore)
+                {
+                    CountMaxScore += 1;
+                    MaxScore = i;
+                }
+
+                if (i < MinScore)
+                {
+                    CountMinScore += 1;
+                    MinScore = i;
+                }
+            }
+
+            List<int> breakingRecords = [CountMinScore, CountMaxScore];
+            return breakingRecords;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@
 // this part is used to pass the parameters to the method called
 //==============================================================
 
-var s = "07:05:45PM";
+var scores = new List<int> { 3, 4, 21, 36, 10, 28, 35, 5, 24, 42 };
 
 //==============================================================
 
 // call the method to want execute
-Console.WriteLine(Answers.TimeConversion(s));
+List<int> Count = Answers.BreakingRecords(scores);
+
+Console.WriteLine($"{Count[0]} {Count[1]}");
