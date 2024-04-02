7f23ccf768fb3a140e5e45ae134c7eb76cd3b08b
Alex Junior <alexjr2oficial@gmail.com>
2024-04-02 11:15:42 -0300

    tentando resolver o desafio FlippingBits

--- Challengers/Challenger.cs
+++ Challengers/Challenger.cs
@@ -8,9 +8,12 @@ public static class Challenger
     // This class will be responsible for calling the challenge methods
     public static void Start()
     {
-        List<int> arr = [0, 1, 2, 4, 6, 5, 3];
-        var n = Median.Start(arr);
+        long MyNumber = 1;
+        // 2147483648 
 
-        Console.WriteLine(n);
+        var c = FlippingBits.Start(MyNumber);
+
+        Console.WriteLine(c);
+
     }
 }
