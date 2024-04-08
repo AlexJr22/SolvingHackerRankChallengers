6bb4f8d86624e82dbc1e0421c9599bdb6bf9491f
Alex Junior <alexjr2oficial@gmail.com>
2024-04-08 13:12:07 -0300

    atualizando Challengers.cs

--- Challengers/Challenger.cs
+++ Challengers/Challenger.cs
@@ -6,23 +6,4 @@ namespace SolvingHackerRankChallengers.Challengers;
 public static class Challenger
 {
     // This class will be responsible for calling the challenge methods
-    public static void Start()
-    {
-        List<List<int>> arr = [
-            [11,2,4],
-            [4,5,6],
-            [10,8,-12],
-        ];
-
-        //List<List<int>> arr = [
-        //    [2],
-        //    [6,8],
-        //    [-6,9],
-        //];
-
-
-        int result = DiagonalDifference.Start(arr);
-
-        Console.WriteLine(result);
-    }
 }
