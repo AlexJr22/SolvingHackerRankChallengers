d3cce8788a275c33d562c2658698e566ffd68099
Alex Junior <alexjr2oficial@gmail.com>
2024-04-08 13:13:01 -0300

    resolvendo o desafio CountingSort

---
+++ Challengers/Answers/CountingSort.cs
@@ -0,0 +1,14 @@
+﻿namespace SolvingHackerRankChallengers.Challengers.Answers;
+
+public static class CountingSort
+{
+    public static List<int> Start(List<int> arr)
+    {
+        int[] nums = new int[100];
+
+        foreach (int i in arr)
+            nums[i] += 1;
+
+        return [ ..nums ];
+    }
+}
